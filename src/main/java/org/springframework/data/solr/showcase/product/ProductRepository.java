4920a27f9e67235746e570f09367084b9cbdafd3
christophstrobl <j73x73r@gmail.com>
2014-01-02 13:46:25 +0100

    STEP-4 - Use Highlight Annotation to mark keywords
    Using Highlight annotation to accentuate search input in result
    documents. Highlighting options can be configured via various annotation
    attribtes. By default all document fields will be target for
    highlighting.

--- src/main/java/org/springframework/data/solr/showcase/product/ProductRepository.java
+++ src/main/java/org/springframework/data/solr/showcase/product/ProductRepository.java
@@ -17,11 +17,12 @@ package org.springframework.data.solr.showcase.product;
 
 import java.util.Collection;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.solr.core.query.Query.Operator;
 import org.springframework.data.solr.core.query.result.FacetPage;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.repository.Facet;
+import org.springframework.data.solr.repository.Highlight;
 import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 import org.springframework.data.solr.showcase.product.model.Product;
@@ -31,10 +32,11 @@ import org.springframework.data.solr.showcase.product.model.Product;
  */
 interface ProductRepository extends SolrCrudRepository<Product, String> {
 
+	@Highlight(prefix = "<b>", postfix = "</b>")
 	@Query(fields = { SearchableProductDefinition.ID_FIELD_NAME, SearchableProductDefinition.NAME_FIELD_NAME,
 			SearchableProductDefinition.PRICE_FIELD_NAME, SearchableProductDefinition.FEATURES_FIELD_NAME,
 			SearchableProductDefinition.AVAILABLE_FIELD_NAME }, defaultOperator = Operator.AND)
-	Page<Product> findByNameIn(Collection<String> names, Pageable page);
+	HighlightPage<Product> findByNameIn(Collection<String> names, Pageable page);
 
 	@Facet(fields = { SearchableProductDefinition.NAME_FIELD_NAME })
 	FacetPage<Product> findByNameStartsWith(Collection<String> nameFragments, Pageable pagebale);
